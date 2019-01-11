4eee48e7151f2de418ab1624c73a97c72b0cb93b
Richard Skogeby <richard.skogeby@outlook.com>
2019-01-11 18:05:37 +0000

    Access first webpage in database

--- get_beamlines.py
+++ get_beamlines.py
@@ -5,16 +5,27 @@ __all__ = []
 __version__ = '0.1'
 __author__ = 'Richard Skogeby'
 
+import sqlite3
+from time import sleep
+
 import asyncio
 from pyppeteer import launch
-
 
 from find_synchrotron_website import *
 from pull_data import *
 from stopwatch import *
 
+
 async def get_beamlines(browser, cur, conn):
-    pass
+    cur.execute('''SELECT external FROM Websites''')
+    websites = cur.fetchall()
+    websites = ['%s' % x for x in websites]
+    website = websites[0]
+    page = await open_page(browser, website)
+    
+
+
+    
 
 
 async def main():
